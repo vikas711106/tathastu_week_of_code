3f815d6e5345e4abe8ed0bed7598b109974fa5ff
vikas711106 <66163728+vikas711106@users.noreply.github.com>
2020-06-05 07:23:02 -0700

    Create 1.py

---
+++ day1/1.py
@@ -0,0 +1,10 @@
+name=input("Enter the name:")
+branch=input("Enter the Branch:")
+gender=input("Enter the Gender:")
+college=input("Enter the College Name:")
+print(name)
+print(branch)
+print(gender)
+print(college)
+
+
